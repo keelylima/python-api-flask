3665e8454066cb75dfa8ceac3ae24ca68f776308
Kelly Lima <keely06@live.com>
2021-02-23 17:07:33 -0300

    create endpoints

--- app.py
+++ app.py
@@ -2,10 +2,45 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My item 1',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
-# função para carregar o que mostra no browser, baseado na rota que passei (app.route)
-def home():
-    return "Hello, world!"
+
+# POST /store data: {name: }
+@app.route('/store', methods=['POST'])
+def create_store():
+        pass
+        
+
+# GET  /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+        pass
+        
+
+# GET /store
+@app.route('/store')
+def get_stores():
+        pass
+        
+
+# POST /store<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+        pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+        pass
 
 app.run(port=5000)
